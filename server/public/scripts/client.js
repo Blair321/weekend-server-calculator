3f1dc0edca1fc896a734404fab87c0dd7b96aa80
Blair Jilek <blairjilek@gmail.com>
2024-11-10 14:45:13 -0600

    made the rest of my functions

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -14,3 +14,32 @@ const sectionView = document.getElementById('mostRecentResult')
 sectionView.innerHTML += `<li> ${numOne} + ${numTwo} = ${numbersAdded} </li>`
 }
 //subtraction function 
+function subtraction(event) {
+    let numOne = Number(document.getElementById('numberOne').value)
+    let numTwo = Number(document.getElementById('numberTwo').value)
+    let numbersSubtracted = numOne - numTwo
+    console.log('Number subtracted',numbersSubtracted);
+    const sectionView = document.getElementById('mostRecentResult')
+sectionView.innerHTML += `<li> ${numOne} - ${numTwo} = ${numbersSubtracted} </li>`
+    
+}
+//multiplication function 
+function multiplication(event) {
+    let numOne = Number(document.getElementById('numberOne').value)
+    let numTwo = Number(document.getElementById('numberTwo').value) 
+    let numbersMultiplied = numOne * numTwo 
+    console.log('Multiplication', numbersMultiplied);
+     const sectionView = document.getElementById('mostRecentResult')
+sectionView.innerHTML += `<li> ${numOne} * ${numTwo} = ${numbersMultiplied} </li>`
+    
+}
+// division
+function division(event) {
+    let numOne = Number(document.getElementById('numberOne').value)
+    let numTwo = Number(document.getElementById('numberTwo').value)
+    let numbersDivided = numOne / numTwo
+    console.log('Number divided',numbersDivided);
+    const sectionView = document.getElementById('mostRecentResult')
+sectionView.innerHTML += `<li> ${numOne} / ${numTwo} = ${numbersDivided} </li>`
+    
+}
