f6cdbc7c50a1a71ab23a75b3e9110b6cc8f1ac42
Blair Jilek <blairjilek@gmail.com>
2024-11-10 13:12:29 -0600

    made chnages to html and made my first function for addition event

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,1 +1,16 @@
 console.log('client.js is sourced!');
+// set reset default
+function onPress(event){
+    event.preventDefault(event)
+    console.log('Prevent the default')}
+// Fist lets make the functions for our operators!!
+function addition(event) {
+    let numOne = Number(document.getElementById('numberOne').value)
+    let numTwo = Number(document.getElementById('numberTwo').value)
+console.log('The Two Numbers:',numOne,numTwo);
+let numbersAdded = numOne + numTwo
+console.log('Both Numbers Addded', numbersAdded);
+const sectionView = document.getElementById('mostRecentResult')
+sectionView.innerHTML += `<li> ${numOne} + ${numTwo} = ${numbersAdded} </li>`
+}
+//subtraction function 
